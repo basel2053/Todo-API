d5e5d7286e0ec3c3a9df0f52a327c4de6921318b
unknown <baselsalah2053@gmail.com>
2023-01-29 01:54:14 +0200

    searching by status

--- src/Controllers/todo.ts
+++ src/Controllers/todo.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
-import Todo from '../Models/todo';
+import Todo, { ITodo } from '../Models/todo';
 
-export const getTodos = async (_req: Request, res: Response) => {
+const todosPerPage = 6;
+
+export const getTodos = async (req: Request, res: Response) => {
 	try {
-		const todos = await Todo.find({ userId: res.locals.userId });
-		res.status(200).json(todos);
+		const page = Number(req.query.page) || 1;
+		const todos = await Todo.find({ userId: res.locals.userId })
+			.skip((page - 1) * todosPerPage)
+			.limit(todosPerPage);
+		const todosCount = await Todo.find({
+			userId: res.locals.userId,
+		}).countDocuments();
+		res.status(200).json({ todos, todosCount, page });
 	} catch (err) {
 		res.status(500).json(err);
 	}
@@ -59,9 +67,28 @@ export const updateTodo = async (req: Request, res: Response) => {
 
 export const search = async (req: Request, res: Response) => {
 	try {
-		const { query } = req.body;
-		const todos = await Todo.find({ title: { $regex: query, $options: 'i' } });
-		res.status(200).json(todos);
+		const page = Number(req.query.page) || 1;
+		const { status } = req.query;
+		let todos: ITodo[];
+		let todosCount: number;
+		if (status) {
+			todosCount = await Todo.find({
+				status: status,
+			}).countDocuments();
+			todos = await Todo.find({ status: status })
+				.skip((page - 1) * todosPerPage)
+				.limit(todosPerPage);
+		} else {
+			const { query } = req.body;
+			todosCount = await Todo.find({
+				title: { $regex: query, $options: 'i' },
+			}).countDocuments();
+			todos = await Todo.find({ title: { $regex: query, $options: 'i' } })
+				.skip((page - 1) * todosPerPage)
+				.limit(todosPerPage);
+		}
+
+		res.status(200).json({ todos, todosCount, page });
 	} catch (err) {
 		res.status(500).json(err);
 	}
